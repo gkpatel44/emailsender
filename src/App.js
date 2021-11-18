c0a004b7fea504b7a494289f18be53c49412bac8
gaurang kathiriya <kathiriyagaurang44@gmail.com>
2021-11-18 16:15:47 +0530

    final commit all done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 import { useState } from 'react';
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [data, setData] = useState({ });
+  const [models,setmodels] = useState(false);
+  const [resp,setResp] = useState("");
 
 
 
@@ -13,37 +17,54 @@ function App() {
 
   }
   const submithandler = (event) => {
+   
     event.preventDefault() 
-    var dataa = {
-      "email": "eve.holt@reqres.in",
-      "password": "cityslicka"
-    }
-
-
-    fetch("https://reqres.in/api/login", {
+    fetch("http://localhost:3030/send", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(dataa)
+      body: JSON.stringify(data)
     })
       .then(resp => resp.json())
-      .then(data => console.log(data))
+      .then(data => {
+     
+      setResp(data)
+      setmodels(true)
+      toast.success(data.message);
+      setmodels(true)
+      setTimeout(() => {
+        setmodels(false)
+      }, 2001);
+    })
       .catch(err => console.log(err))
   }
   return (
+    models?
+    <ToastContainer
+position="top-center"
+autoClose={2000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+/>
+    :
     <div className="container">
       <h1>Enquiry Form </h1>
       <form id="form" onSubmit={submithandler}>
 
         <label htmlFor="fname">FullName:
         <input
-          name="fname"
+          name="name"
           type="text"
           minLength="3"
           onChange={inputhandler}
         /></label>
         <br />
 
-        <label htmlFor="fname">Email:
+        <label htmlFor="name">Email:
         <input
           name="email"
           type="email"
@@ -64,7 +85,7 @@ function App() {
 
         <label htmlFor="msg">Messsage:
         <input
-          name="msg"
+          name="message"
           minLength="6"
           onChange={inputhandler}
         />
